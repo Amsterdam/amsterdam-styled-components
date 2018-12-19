12ddf25fecf15582c38bdc82feb9cfeb0fe01b27
Jasper Swart <jasper.g.swart@gmail.com>
2018-12-19 11:25:11 +0100

    added disabled option to radio

--- packages/asc-core/src/Radio/Radio.tsx
+++ packages/asc-core/src/Radio/Radio.tsx
@@ -6,25 +6,70 @@ const Radio = styled.input.attrs({
 })`
   cursor: pointer;
   height: 28px;
+  left: 0;
   margin: 0;
+  opacity: 0;
+  position: absolute;
+  top: 0;
   width: 28px;
 `
 
 const Label = styled.label`
+  color: ${props => props.disabled ? '#bebebe' : '#000'};
+  padding-left: 38px;
+  line-height: 28px;
+
+  &:before {
+    border-radius: 50%;
+    border: ${props => props.disabled ? '1px solid #bebebe' : '1px solid #000'};
+    content: "";
+    height: calc(28px - 2px);
+    left: 0;
+    position: absolute;
+    top: 0;
+    width: calc(28px - 2px);
+  }
+
+  &:after {
+    background-color: #0000;
+    border: 6px solid;
+    border-radius: 50%;
+    color: ${props => props.disabled ? '#bebebe' : '#000'};
+    content: "";
+    height: 0;
+    left: 7px;
+    opacity: 0;
+    position: absolute;
+    top: 7px;
+    width: 0;
+    zoom: 1;
+  }
 `
 
-const Wrapper = (props: any) => (
-  <div>
+const Wrapper = styled.div`
+  position: relative;
+  margin-bottom: 10px;
+
+  input:checked + label:after {
+    opacity: 1;
+  }
+`
+
+const InputRadio = (props: any) => (
+  <Wrapper>
     <Radio
       id={props.id}
       name={props.name}
       value={props.value}
+      defaultChecked={props.defaultChecked}
+      disabled={props.disabled}
     />
     <Label
       htmlFor={props.id}
+      disabled={props.disabled}
     >{props.label}
     </Label>
-  </div>
+  </Wrapper>
 )
 
-export default Wrapper
+export default InputRadio
--- packages/asc-ui/src/Radio/Radio.stories.tsx
+++ packages/asc-ui/src/Radio/Radio.stories.tsx
@@ -4,7 +4,8 @@ import { Radio } from '@amsterdam/asc-core'
 
 storiesOf('Radio', module).add('All Radios', () => (
   <div>
-    <Radio id="radio-1" name="gender" value="female" label="Vrouw" />
+    <Radio id="radio-1" name="gender" value="female" label="Vrouw" defaultChecked />
     <Radio id="radio-2" name="gender" value="male" label="Man" />
+    <Radio id="radio-3" name="gender" value="none" label="Onzijdig" disabled />
   </div>
 ))
