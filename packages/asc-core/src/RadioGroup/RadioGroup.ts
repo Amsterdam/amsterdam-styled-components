88640a9be47e7aef98a8f1fceeb96204e575f2c1
Jasper Swart <jasper.g.swart@gmail.com>
2019-02-04 11:12:25 +0100

    added radiogroup with orientation

--- packages/asc-core/src/RadioGroup/RadioGroup.ts
+++ packages/asc-core/src/RadioGroup/RadioGroup.ts
@@ -1,7 +1,11 @@
 import styled from 'styled-components'
+import RadioItem from '../RadioItem'
 
 const RadioGroup = styled.div`
-  display: ${(props) => props.orientation === 'horizontal' ? 'inline-block' : 'block'};
+  ${RadioItem} {
+    display: ${(props) => props.orientation === 'horizontal' ? 'inline-block' : 'block'};
+    margin-right: ${(props) => props.orientation === 'horizontal' ? '24px' : '0'};
+  }
 `
 
 export default RadioGroup
