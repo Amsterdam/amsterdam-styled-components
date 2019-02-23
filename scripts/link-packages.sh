325683aeb51373c7fc090e53eb239fd60794efa6
Chris van Mook <chris@chrisvanmook.com>
2019-02-23 10:39:38 +0100

    Added bash script to yarn link all packages

---
+++ scripts/link-packages.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+current_path=`pwd`
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+cd "$parent_path"
+
+PACKAGE_NAMES=()
+
+for dir in ../packages/*/
+do
+    cd $dir
+    yarn link
+    PACKAGE_NAME=$(cat package.json | grep name | head -1 | sed 's/\"name\"://g' | tr -d '"'  | tr -d ',')
+    PACKAGE_NAMES+=($PACKAGE_NAME)
+    cd ../
+done
+
+cd "$current_path"
+
+for package_name in "${PACKAGE_NAMES[@]}"
+do
+    npm link $package_name
+done
