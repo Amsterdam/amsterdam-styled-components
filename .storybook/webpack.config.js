f69328946dba7c3c7c4866bebd85f2c7f3be5a4e
Chris van Mook <chris@chrisvanmook.com>
2019-02-25 14:28:36 +0100

    Fixed issue with storybook / tsconfig

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,21 +2,25 @@ const path = require('path')
 
 const basePath = path.resolve(__dirname, '../', 'packages')
 
-module.exports = (storybookBaseConfig, configType, defaultConfig) => {
+module.exports = (baseConfig, mode, defaultConfig) => {
   // Add typescript support
   defaultConfig.module.rules.push({
     test: /\.(ts|tsx)$/,
     use: [{
       loader: require.resolve('awesome-typescript-loader'),
-    }],
+      options: {
+        configFileName: './tsconfig.storybook.json',
+      },
+    },
+    ],
   })
   defaultConfig.resolve.extensions.push('.ts', '.tsx')
 
-  // Since we use the react-scripts webpack config, that only looks at the /src folder,
+  // Since we use the react-scripts webpack defaultConfig, that only looks at the /src folder,
   // we need to add our ./package directory.
   defaultConfig.module.rules = defaultConfig.module.rules.map((rule) => ({
     ...rule,
-    ...(rule.include) ? { include: [...rule.include, basePath] } : {},
+    ...(Array.isArray(rule.include)) ? { include: [...rule.include, basePath] } : {},
   }))
   return defaultConfig
 }
