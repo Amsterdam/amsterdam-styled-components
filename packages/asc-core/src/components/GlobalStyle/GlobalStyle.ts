212866ddbf24e3dc74ea8d8be515d8ef90c878e5
Chris van Mook <chris@chrisvanmook.com>
2019-02-25 14:57:07 +0100

    Fixed lint issues, removed unused deps

--- packages/asc-core/src/components/GlobalStyle/GlobalStyle.ts
+++ packages/asc-core/src/components/GlobalStyle/GlobalStyle.ts
@@ -5,7 +5,8 @@ import { THEME_NAME } from '../../theme'
 export default createGlobalStyle`
   ${normalize()}
   ${({ theme }) => theme[THEME_NAME].globalStyle}
+
   body {
-    font-family: ${({ theme }) => theme[THEME_NAME].typography.primary}
+    font-family: ${({ theme }) => theme[THEME_NAME].typography.primary};
   }
 `
