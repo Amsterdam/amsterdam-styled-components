2ebf1d3413b53faeb91cbc5d7fec9ac80bf0c238
Chris van Mook <chris@chrisvanmook.com>
2019-03-01 13:22:43 +0100

    TSLint -> ESLint

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path')
+const { lstatSync, readdirSync } = require('fs')
 
 const basePath = path.resolve(__dirname, '../', 'packages')
+const packages = readdirSync(basePath).filter((name) =>
+  lstatSync(path.join(basePath, name)).isDirectory(),
+)
 
 module.exports = (baseConfig, mode, defaultConfig) => {
   // Add typescript support
@@ -11,10 +15,17 @@ module.exports = (baseConfig, mode, defaultConfig) => {
       options: {
         configFileName: './tsconfig.storybook.json',
       },
-    },
-    ],
+    }],
   })
   defaultConfig.resolve.extensions.push('.ts', '.tsx')
+
+  // Resolve every package to it's src directory
+  Object.assign(defaultConfig.resolve.alias, {
+    ...packages.reduce((acc, name) => ({
+      ...acc,
+      [`@datapunt/${name}`]: path.join(basePath, name, name !== 'asc-assets' ? '/src' : ''),
+    }), {}),
+  })
 
   // Since we use the react-scripts webpack defaultConfig, that only looks at the /src folder,
   // we need to add our ./package directory.
