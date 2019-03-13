0f6387735ff74024d2848de420cf73631bb3608a
Chris van Mook <chris@chrisvanmook.com>
2019-03-13 10:30:40 +0100

    Added example of usage of modal in storybook

--- packages/asc-ui/src/components/Modal/Modal.stories.tsx
+++ packages/asc-ui/src/components/Modal/Modal.stories.tsx
@@ -2,8 +2,18 @@ import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 import { linkTo } from '@storybook/addon-links'
 import { withKnobs, number } from '@storybook/addon-knobs'
+import { action } from '@storybook/addon-actions'
+import { ReactComponent as Close } from '@datapunt/asc-assets/lib/Icons/Close.svg'
 import { Props } from './Modal'
-import { ListItem, Modal, Typography } from '../..'
+import {
+  ListItem,
+  Modal,
+  Typography,
+  Button,
+  Divider,
+  IconButton,
+  TopBar,
+} from '../..'
 
 const SimpleModal: React.FC<Props> = props => (
   <div>
@@ -27,24 +37,70 @@ storiesOf('Modal', module)
   .addDecorator(withKnobs)
   .add('default state', () => <SimpleModal open />)
   .add('backdrop opacity', () => (
-    <>
-      <SimpleModal
-        open
-        backdropOpacity={number('backdropOpacity', 0.2, {
-          range: true,
-          min: 0,
-          max: 1,
-          step: 0.1,
-        })}
-      />
-    </>
+    <SimpleModal
+      open
+      backdropOpacity={number('backdropOpacity', 0.2, {
+        range: true,
+        min: 0,
+        max: 1,
+        step: 0.1,
+      })}
+    />
+  ))
+  .add('example of actual usage', () => (
+    <Modal
+      open
+      aria-labelledby="feedback"
+      aria-describedby="feedback"
+      onClose={action('close modal from escape button or click outside')}
+      blurredNode={window.document.querySelector('#root') as HTMLInputElement}
+    >
+      <TopBar>
+        <Typography style={{ flexGrow: 1 }} element="h4">
+          Feedback
+          <IconButton onClick={action('close modal')}>
+            <Close />
+          </IconButton>
+        </Typography>
+      </TopBar>
+      <Divider />
+      <ListItem>
+        <Typography gutterBottom element="h5">
+          Onjuiste of ontbrekende gegevens?
+        </Typography>
+        <Typography paragraph element="p" gutterBottom>
+          Geef aan welke gegevens onjuist zijn of ontbreken. Ook als je weet wat
+          het wel moet zijn. We horen het graag.
+        </Typography>
+        <Button as="a" color="primary" onClick={action('click')}>
+          Terugmelden
+        </Button>
+      </ListItem>
+      <Divider gutter />
+      <ListItem>
+        <Typography gutterBottom element="h5">
+          Vraag of een klacht?
+        </Typography>
+        <Typography paragraph element="p" gutterBottom>
+          Als iets op deze pagina niet goed werkt, onduidelijk is of vragen
+          oproept, geef het aan ons door.
+        </Typography>
+        <Button as="a" color="primary" onClick={action('click')}>
+          Probleem melden
+        </Button>
+      </ListItem>
+      <Divider transparent />
+      <ListItem>
+        <Typography element="a" href="#">
+          Hulp nodig?
+        </Typography>
+      </ListItem>
+    </Modal>
   ))
   .add('with blurred background', () => (
-    <>
-      <SimpleModal
-        open
-        blurredNode={window.document.querySelector('#root') as HTMLInputElement}
-      />
-    </>
+    <SimpleModal
+      open
+      blurredNode={window.document.querySelector('#root') as HTMLInputElement}
+    />
   ))
   .add('closed state', () => <SimpleModal open={false} />)
