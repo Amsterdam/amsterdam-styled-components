9339c6a10cc47d00c6f40bdbc9b8928131ad1f48
Jasper Swart <jasper.g.swart@gmail.com>
2019-04-01 21:17:08 +0200

    refactored radios in 2 storybook items

--- packages/asc-ui/src/components/Radio/Radio.stories.tsx
+++ packages/asc-ui/src/components/Radio/Radio.stories.tsx
@@ -2,30 +2,34 @@ import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 import Radio, { RadioGroup } from './Radio'
 
-storiesOf('Radio', module).add('All radios', () => (
-  <div>
-    <RadioGroup>
-      <Radio id="female" name="gender" value="female" label="Vrouw" />
-      <Radio id="male" name="gender" value="male" label="Man" />
-      <Radio
-        id="none"
-        name="gender"
-        value="none"
-        label="Geen van beide"
-        disabled
-      />
-    </RadioGroup>
-
-    <RadioGroup orientation="horizontal">
-      <Radio id="female2" name="gender2" value="female" label="Vrouw 2" />
-      <Radio id="male2" name="gender2" value="male" label="Man 2" />
-      <Radio
-        id="none2"
-        name="gender2"
-        value="none"
-        label="Geen van beide"
-        disabled
-      />
-    </RadioGroup>
-  </div>
-))
+storiesOf('Radio', module)
+  .add('Vertical (default)', () => (
+    <div>
+      <RadioGroup>
+        <Radio id="female" name="gender" value="female" label="Vrouw" />
+        <Radio id="male" name="gender" value="male" label="Man" />
+        <Radio
+          id="none"
+          name="gender"
+          value="none"
+          label="Geen van beide"
+          disabled
+        />
+      </RadioGroup>
+    </div>
+  ))
+  .add('Horizontal', () => (
+    <div>
+      <RadioGroup orientation="horizontal">
+        <Radio id="female2" name="gender2" value="female" label="Vrouw 2" />
+        <Radio id="male2" name="gender2" value="male" label="Man 2" />
+        <Radio
+          id="none2"
+          name="gender2"
+          value="none"
+          label="Geen van beide"
+          disabled
+        />
+      </RadioGroup>
+    </div>
+  ))
