1527d0aacbb621ba3ae06714921bcb74ae19b32f
royderks <10717410+royderks@users.noreply.github.com>
2019-04-12 09:37:30 +0200

    Added more information about integrating components from this package with webpack

--- packages/asc-core/src/components/ContextMenu/ContextMenuWrapper.ts
+++ packages/asc-core/src/components/ContextMenu/ContextMenuWrapper.ts
@@ -5,6 +5,10 @@ const Wrapper = styled.div<ContextMenu.WrapperProps>`
   display: flex;
   align-items: flex-start;
   flex-direction: column;
+
+  [aria-hidden='true'] {
+    display: none;
+  }
 `
 
 export default Wrapper
