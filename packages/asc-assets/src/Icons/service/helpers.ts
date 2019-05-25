5bf41673fbb93498c2a0e34be5ca54c8a5aef9b6
Adrian Tudorache <a.p.tudorache@gmail.com>
2019-05-25 09:16:27 +0200

    icons: update svg's

---
+++ packages/asc-assets/src/Icons/service/helpers.ts
@@ -0,0 +1,38 @@
+
+import { AbstractNode, HelperRenderOptions, IconDefinition } from '../types'
+
+export function renderIconDefinitionToSVGElement(
+  icon: IconDefinition,
+  options: HelperRenderOptions = {},
+): string {
+  return renderAbstractNodeToSVGElement(icon.icon, options)
+}
+
+function renderAbstractNodeToSVGElement(
+  node: AbstractNode,
+  options: HelperRenderOptions,
+): string {
+  const targetAttrs =
+    node.tag === 'svg'
+      ? {
+          ...node.attrs,
+          ...(options.extraSVGAttrs || {}),
+        }
+      : node.attrs
+  const attrs = Object.keys(targetAttrs).reduce((acc: string[], nextKey) => {
+    const key = nextKey
+    const value = targetAttrs[key]
+    const token = `${key}="${value}"`
+    acc.push(token)
+    return acc
+  }, [])
+  const attrsToken = attrs.length ? ` ${attrs.join(' ')}` : ''
+  const container: [string, string] = [
+    `<${node.tag}${attrsToken}>`,
+    `</${node.tag}>`,
+  ]
+  const children = (node.children || [])
+    .map(child => renderAbstractNodeToSVGElement(child, options))
+    .join('')
+  return `${container[0]}${children}${container[1]}`
+}
