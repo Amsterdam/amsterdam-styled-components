bc05b977b4917c6a837cd2a246e5080940c0d838
royderks <10717410+royderks@users.noreply.github.com>
2019-05-28 16:06:02 +0200

    Fixed linting issues

--- packages/asc-core/src/theme/index.ts
+++ packages/asc-core/src/theme/index.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-useless-constructor, no-empty-function */
+/* eslint-disable no-useless-constructor, no-empty-function */
 import { CSSProp } from 'styled-components'
 
 export namespace Theme {
@@ -110,11 +110,6 @@ export namespace Theme {
       public colors: ColorInterface,
       public globalStyle: GlobalStyleType,
       public typography: TypographyInterface,
-    ) {
-      this.breakpoints = breakpoints
-      this.colors = colors
-      this.globalStyle = globalStyle
-      this.typography = typography
-    }
+    ) {}
   }
 }
