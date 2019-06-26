550c9dec738cd210008f7b34241c28d0f122aecb
royderks <10717410+royderks@users.noreply.github.com>
2019-06-26 10:31:06 +0200

    Small changes for aligning the search bar and logo in the small header

--- packages/asc-ui/src/components/Header/HeaderStyle.ts
+++ packages/asc-ui/src/components/Header/HeaderStyle.ts
@@ -1,5 +1,4 @@
 import styled from '@datapunt/asc-core'
-import { breakpoint } from '../../utils'
 import TopBarStyle from '../TopBar/TopBarStyle'
 import { HEADER_MAX_WIDTH } from './sizes'
 
@@ -11,13 +10,12 @@ const HeaderStyle = styled(TopBarStyle)<HeaderStyleProps>`
   flex-wrap: nowrap;
   height: 100%;
   margin: 0 auto;
+  padding: 0 10px;
   align-items: stretch;
   max-width: ${({ fullWidth }) =>
     !fullWidth ? `${HEADER_MAX_WIDTH}px` : 'none'};
-
-  @media screen and ${breakpoint('max-width', 'laptopM')} {
-    padding: 0 10px;
-  }
+  
+  ${({ fullWidth }) => `padding-left: 10px !important;`}
 `
 
 export default HeaderStyle
