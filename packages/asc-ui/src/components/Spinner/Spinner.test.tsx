d0da06e1d74ae508c49560fec18431cc57b16d77
royderks <10717410+royderks@users.noreply.github.com>
2019-07-01 17:41:21 +0200

    Added spinner component with tests

---
+++ packages/asc-ui/src/components/Spinner/Spinner.test.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react'
+import { shallow } from 'enzyme'
+import { Spinner as SpinnerIcon } from '@datapunt/asc-assets'
+
+import Spinner from './Spinner'
+import Icon from '../Icon'
+
+describe('Spinner', () => {
+  it('should render the Spinner', () => {
+    const component = shallow(<Spinner />).dive()
+    const icon = component.find(Icon).at(0)
+    expect(component.at(0).exists()).toBeTruthy()
+
+    expect(icon.props().color).toBe('default')
+
+    expect(icon.props().size).toBe(20)
+
+    expect(
+      component
+        .at(0)
+        .find(SpinnerIcon)
+        .exists(),
+    ).toBeTruthy()
+  })
+
+  it('should set a size for the Spinner', () => {
+    const component = shallow(<Spinner size={50} />).dive()
+
+    expect(
+      component
+        .find(Icon)
+        .at(0)
+        .props().size,
+    ).toBe(50)
+  })
+
+  it('should set a color for the Spinner', () => {
+    const component = shallow(<Spinner color="secondary" />).dive()
+
+    expect(
+      component
+        .find(Icon)
+        .at(0)
+        .props().color,
+    ).toBe('secondary')
+  })
+})
