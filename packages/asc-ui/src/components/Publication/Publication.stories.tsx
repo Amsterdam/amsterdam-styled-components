c5f2cb4bbcc53cd4f4be3dd58df74497d26266d0
Adrian Tudorache <a.p.tudorache@gmail.com>
2019-07-11 08:21:58 +0200

    added intro to the publication component

--- packages/asc-ui/src/components/Publication/Publication.stories.tsx
+++ packages/asc-ui/src/components/Publication/Publication.stories.tsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react/'
+import { action } from '@storybook/addon-actions'
 import { Column, CustomHTMLBlock, Row, ArticleMetaList } from '../../index'
 import Summary from '../Typography/Summary'
 import ArticleHeader from '../Article/ArticleHeader/ArticleHeader'
-import publicationJSON from './publication_feed.json'
 import Downloader from '../Downloader'
 import PublicationContent from './PublicationContent'
 import Publication from './Publication'
+import publicationJSON from './publication_feed.json'
 
 storiesOf('Composed/Publication', module).add('with image', () => {
   const {
     title,
     // created,
     body,
+    field_publication_intro: intro,
     field_file_size: fileSize,
     field_file_type: fileType,
-  } = publicationJSON.attributes
+  } = publicationJSON.data[0].attributes
+  const cmsRoot = 'https://acc.cms.data.amsterdam.nl'
+  const coverUrl = publicationJSON.included[0].attributes.uri.url
+  const downloadUrl = publicationJSON.included[1].attributes.uri.url
+
   return (
     <>
       <br />
@@ -41,19 +47,14 @@ storiesOf('Composed/Publication', module).add('with image', () => {
             </Column>
             <Column span={{ small: 1, medium: 4, big: 3, large: 6, xLarge: 6 }}>
               <Downloader
-                imageSrc="https://acc.cms.data.amsterdam.nl/sites/default/files/images/2019-factsheet-jeugdwerkloosheid-2014-2018.png"
+                imageSrc={`${cmsRoot}${coverUrl}`}
                 description={`Download PDF (${fileSize})`}
-                onClick={() => console.log('download')}
+                onClick={action(`downloading: ${cmsRoot}${downloadUrl}`)}
               />
             </Column>
             <Column span={{ small: 1, medium: 4, big: 3, large: 6, xLarge: 6 }}>
               <PublicationContent>
-                <Summary>
-                  Optioneel hier kan een kleine omschrijving komen. Van de
-                  146.500 Amsterdamse jongeren in de leeftijd van 15 tot en met
-                  26 jaar waren er in 2018 gemiddeld 6.100 werkloos, dit komt
-                  neer op 6,2% van de beroepsbevolking.
-                </Summary>
+                <Summary>{intro}</Summary>
                 <CustomHTMLBlock
                   dangerouslySetInnerHTML={{
                     __html: body.processed,
