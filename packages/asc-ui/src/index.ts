06cfa9d45b092d15d4c553fd80fdbba8e1cdd764
royderks <10717410+royderks@users.noreply.github.com>
2019-07-17 14:27:50 +0200

    Added intial code for the card component, created index file for spinner

--- packages/asc-ui/src/index.ts
+++ packages/asc-ui/src/index.ts
@@ -1,65 +1,53 @@
-import { ServerStyleSheet, Theme, ascDefaultTheme } from '@datapunt/asc-core'
-
+import { ascDefaultTheme, ServerStyleSheet, Theme } from '@datapunt/asc-core'
 import AmsterdamLogo, { AmsterdamLogoStyles } from './components/AmsterdamLogo'
 import BackDrop, { BackDropStyles } from './components/BackDrop'
-import IconButton from './components/IconButton'
-import Icon from './components/Icon'
-import Input, { InputStyles } from './components/Input'
-import Modal from './components/Modal'
-import Portal from './components/Portal'
-import Typography from './components/Typography'
-import TopBar, { TopBarStyles } from './components/TopBar'
-import ShareBar from './components/ShareBar'
-import ShareButton from './components/ShareButton'
 import Button from './components/Button'
-import Radio from './components/Radio'
-import Focus from './components/Focus'
+import Card from './components/Card'
 import ContextMenu from './components/ContextMenu'
 import ContextMenuButtonStyle from './components/ContextMenu/ContextMenuButton'
 import ContextMenuItem from './components/ContextMenu/ContextMenuItem'
-import Header, { HeaderStyles } from './components/Header'
 import Divider from './components/Divider'
+import Focus from './components/Focus'
+import GlobalStyle from './components/GlobalStyle'
+import { Column, GridContainer, GridItem, Row } from './components/Grid'
+import Header, { HeaderStyles } from './components/Header'
+import Icon from './components/Icon'
+import IconButton from './components/IconButton'
+import Input, { InputStyles } from './components/Input'
 import ListItem from './components/ListItem'
+import {
+  MenuFlyOut,
+  MenuInline,
+  MenuItem,
+  MenuItemLabel,
+  MenuItemLink,
+  MenuItemTitle,
+  MenuStyles,
+  MenuToggle,
+} from './components/Menu'
+import Modal from './components/Modal'
+import Portal from './components/Portal'
+import Radio from './components/Radio'
 import SearchBar, { SearchBarStyles } from './components/SearchBar'
 import SearchBarToggle, {
   SearchBarToggleStyles,
 } from './components/SearchBarToggle'
+import ShareBar from './components/ShareBar'
+import ShareButton from './components/ShareButton'
+import Spinner from './components/Spinner'
 import TextField, { TextFieldStyles } from './components/TextField'
-import GlobalStyle from './components/GlobalStyle'
-import { GridContainer, GridItem, Column, Row } from './components/Grid'
-import {
-  MenuItemTitle,
-  MenuItemLink,
-  MenuItemLabel,
-  MenuItem,
-  MenuFlyOut,
-  MenuInline,
-  MenuToggle,
-  MenuStyles,
-} from './components/Menu'
+import TopBar, { TopBarStyles } from './components/TopBar'
+import Typography from './components/Typography'
 import ThemeProvider from './theme'
-import Spinner from './components/Spinner/Spinner'
 
 export { breakpoint } from './utils'
-
-export const styles = {
-  ...AmsterdamLogoStyles,
-  ...BackDropStyles,
-  ...HeaderStyles,
-  ...SearchBarStyles,
-  ...TextFieldStyles,
-  ...TopBarStyles,
-  ...InputStyles,
-  ...SearchBarToggleStyles,
-  ...MenuStyles,
-}
-
 // Components
 export {
   AmsterdamLogo,
   ascDefaultTheme,
   BackDrop,
   Button,
+  Card,
   Column,
   ContextMenu,
   ContextMenuButtonStyle,
@@ -96,4 +84,16 @@ export {
   ThemeProvider,
   TopBar,
   Typography,
+}
+
+export const styles = {
+  ...AmsterdamLogoStyles,
+  ...BackDropStyles,
+  ...HeaderStyles,
+  ...SearchBarStyles,
+  ...TextFieldStyles,
+  ...TopBarStyles,
+  ...InputStyles,
+  ...SearchBarToggleStyles,
+  ...MenuStyles,
 }
