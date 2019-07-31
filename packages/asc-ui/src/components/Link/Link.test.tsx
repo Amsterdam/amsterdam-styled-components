9faae8bf5dc1fed63421a60f5e73c34bc9ead56e
Adrian Tudorache <a.p.tudorache@gmail.com>
2019-07-31 10:31:49 +0200

    added tests to the link component

--- packages/asc-ui/src/components/Link/Link.test.tsx
+++ packages/asc-ui/src/components/Link/Link.test.tsx
@@ -1,16 +1,50 @@
 import * as React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import { ascDefaultTheme, ThemeProvider } from '@datapunt/asc-core'
 import Link from './Link'
-import { renderWithTheme } from '../../utils/withTheme'
 import 'jest-styled-components'
 
+const theme = { ...ascDefaultTheme }
+
 describe('Link', () => {
-  it('should render', () => {
-    const component = renderWithTheme(
-      <Link href="/" linkType="inline">
-        Foo
-      </Link>,
+  afterEach(cleanup)
+
+  it('should render the links ', () => {
+    const { getByText } = render(
+      <ThemeProvider theme={theme}>
+        <>
+          <Link href="http://default-link">default link</Link>
+          <Link href="http://with-chevron-link" linkType="with-chevron">
+            default link with chevron
+          </Link>
+          <Link href="http://inline-link" linkType="inline">
+            inline link
+          </Link>
+          <Link href="http://blank-link" linkType="blank">
+            blank link (no style)
+          </Link>
+        </>
+      </ThemeProvider>,
     )
 
-    expect(component).toMatchSnapshot()
+    const anchorDefault = getByText('default link').closest('a')
+    expect(anchorDefault && anchorDefault.getAttribute('href')).toEqual(
+      'http://default-link',
+    )
+
+    const anchorWithChevron = getByText(/with chevron/).closest('a')
+    expect(anchorWithChevron && anchorWithChevron.getAttribute('href')).toEqual(
+      'http://with-chevron-link',
+    )
+
+    const anchorInline = getByText('inline link').closest('a')
+    expect(anchorInline && anchorInline.getAttribute('href')).toEqual(
+      'http://inline-link',
+    )
+
+    const anchorBlank = getByText(/blank link/).closest('a')
+    expect(anchorBlank && anchorBlank.getAttribute('href')).toEqual(
+      'http://blank-link',
+    )
   })
 })
