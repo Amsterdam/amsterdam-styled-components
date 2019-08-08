c84fc4e73e5db76b8ab274cd16f9a4a477c18863
royderks <10717410+royderks@users.noreply.github.com>
2019-08-08 13:19:29 +0200

    Added test for the open and onOpen props

--- packages/asc-ui/src/components/Toggle/Toggle.test.tsx
+++ packages/asc-ui/src/components/Toggle/Toggle.test.tsx
@@ -1,29 +1,26 @@
 import * as React from 'react'
-import { mount, render } from 'enzyme'
+import { mount } from 'enzyme'
 import Toggle from './Toggle'
 import 'jest-styled-components'
 import { KeyboardKeys } from '../../types'
 import ToggleButton from '../Button/ToggleButton/ToggleButton'
 
 describe('Toggle', () => {
-  it('should render', () => {
-    const component = render(
-      <Toggle>
-        <div id="child">Foo</div>
-      </Toggle>,
-    )
-
-    expect(component).toMatchSnapshot()
-  })
-
-  it('should display the children when the button is clicked', () => {
-    const onClickMock = jest.fn()
-    const component = mount(
+  let component: any
+  const onClickMock = jest.fn()
+  beforeEach(() => {
+    component = mount(
       <Toggle onClick={onClickMock} render={false}>
         <div id="child">Foo</div>
       </Toggle>,
     )
+  })
 
+  it('should render', () => {
+    expect(component).toMatchSnapshot()
+  })
+
+  it('should display the children when the button is clicked', () => {
     expect(component.find('#child').exists()).toBe(false)
     component
       .find(ToggleButton)
@@ -34,16 +31,10 @@ describe('Toggle', () => {
   })
 
   it('should not render the children when user presses the escape key after opening it', () => {
-    const onClickMock = jest.fn()
     const escape = {
       preventDefault: () => {},
       key: KeyboardKeys.Escape,
     }
-    const component = mount(
-      <Toggle onClick={onClickMock} render={false}>
-        <div id="child">Foo</div>
-      </Toggle>,
-    )
 
     component
       .find(ToggleButton)
@@ -53,5 +44,30 @@ describe('Toggle', () => {
     component.simulate('keydown', escape)
 
     expect(component.find('#child').exists()).toBe(false)
+  })
+
+  it('should call the passed parent function when the button is clicked', () => {
+    const onOpenMock = jest.fn()
+    component = mount(
+      <Toggle onClick={jest.fn()} onOpen={onOpenMock} render={false}>
+        <div id="child">Foo</div>
+      </Toggle>,
+    )
+
+    component
+      .find(ToggleButton)
+      .at(0)
+      .simulate('click')
+    expect(onOpenMock).toHaveBeenCalledWith(true)
+  })
+
+  it('should display the children when the parent passes an open prop', () => {
+    component = mount(
+      <Toggle onClick={jest.fn()} open render={false}>
+        <div id="child">Foo</div>
+      </Toggle>,
+    )
+
+    expect(component.find('#child').exists()).toBe(true)
   })
 })
