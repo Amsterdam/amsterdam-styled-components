f0a09d9d34e1a60c9ffc08c86af2fab4679fc1a0
Chris van Mook <chris@chrisvanmook.com>
2019-08-21 11:43:50 +0200

    Added ie11 fix for FooterContent

--- packages/asc-ui/src/components/Footer/FooterContent/FooterContentStyle.ts
+++ packages/asc-ui/src/components/Footer/FooterContent/FooterContentStyle.ts
@@ -8,6 +8,7 @@ export interface Props extends ShowHideTypes {
 
 export default styled.div<Props>`
   display: flex;
+  flex: 1;
   flex-direction: column;
   margin-left: ${({ indent }) => (indent ? '28px' : '0')};
   ${showHide}
