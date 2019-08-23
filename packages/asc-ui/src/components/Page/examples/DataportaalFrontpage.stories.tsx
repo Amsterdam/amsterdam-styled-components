250fc6023704da429799860fa90bb26d2f3afae9
Adrian Tudorache <a.p.tudorache@gmail.com>
2019-08-23 08:36:39 +0200

    DP-7094 added frontpage subtitle

--- packages/asc-ui/src/components/Page/examples/DataportaalFrontpage.stories.tsx
+++ packages/asc-ui/src/components/Page/examples/DataportaalFrontpage.stories.tsx
@@ -1,25 +1,44 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { DataportaalHeader } from '../../Header/examples/DataportaalHeader.stories'
+import styled from '@datapunt/asc-core'
+import {
+  DataportaalHeader,
+  DataportaalBody,
+} from '../../Header/examples/DataportaalHeader.stories'
 import { DataportaalFooter } from '../../Footer/Footer.stories'
-import Row from '../Row'
-import { Column } from '..'
 import { DataportaalCardSpecials } from '../../Card/examples/CardSpecials.stories'
 import { DataportaalCardGenericFunctions } from '../../Card/examples/CardGenericFunctions.stories'
 import { DataportaalCardArticles } from '../../Card/examples/CardArticles.stories'
 import { DataportaalCardAboutData } from '../../Card/examples/CardAboutData.stories'
 import { DataportaalImageCard } from '../../ImageCard/ImageCard.stories'
+import { Row, Column } from '../../Grid'
+import { Heading } from '../../..'
+import { breakpoint } from '../../../utils'
+import Link from '../../Link'
 
-storiesOf('Composed/Frontpage', module).add(
-  'implementation for dataportaal',
-  () => (
-    <>
-      <Row hasMargin={false}>
+const DataportaalSubtiltle = styled(Heading)`
+  padding-top: 56px;
+
+  @media screen and ${breakpoint('max-width', 'laptopM')} {
+    padding-top: 16px;
+  }
+`
+
+const OverviewLink = styled(Link)`
+  margin-top: 32px;
+  padding: 8px 3px 8px 0;
+`
+
+storiesOf('Composed/Page', module).add('implementation for dataportaal', () => (
+  <>
+    <DataportaalHeader tall fullWidth={false} />
+    <DataportaalBody tall fullWidth={false}>
+      <Row>
         <Column span={{ small: 1, medium: 2, big: 6, large: 12, xLarge: 12 }}>
-          <DataportaalHeader tall fullWidth />
+          <DataportaalSubtiltle>Uitgelicht</DataportaalSubtiltle>
         </Column>
       </Row>
-      <Row hasMargin={false}>
+      <Row>
         <Column
           wrap
           span={{ small: 1, medium: 2, big: 6, large: 7, xLarge: 7 }}
@@ -29,9 +48,13 @@ storiesOf('Composed/Frontpage', module).add(
         <Column
           wrap
           span={{ small: 1, medium: 2, big: 6, large: 5, xLarge: 5 }}
-          // push={{ small: 0, medium: 0, big: 0, large: 7, xLarge: 7 }}
         >
           <DataportaalCardGenericFunctions />
+        </Column>
+        <Column span={{ small: 1, medium: 2, big: 6, large: 12, xLarge: 12 }}>
+          <OverviewLink linkType="with-chevron" href="/">
+            Bekijk overzicht
+          </OverviewLink>
         </Column>
       </Row>
       <Row hasMargin={false}>
@@ -44,16 +67,12 @@ storiesOf('Composed/Frontpage', module).add(
           <DataportaalCardAboutData />
         </Column>
       </Row>
-      <Row hasMargin={false}>
+      <Row hasMargin={true}> 
         <Column span={{ small: 1, medium: 2, big: 6, large: 12, xLarge: 12 }}>
           <DataportaalCardArticles />
         </Column>
       </Row>
-      <Row hasMargin={false}>
-        <Column span={{ small: 1, medium: 2, big: 6, large: 12, xLarge: 12 }}>
-          <DataportaalFooter />
-        </Column>
-      </Row>
-    </>
-  ),
-)
+      <DataportaalFooter />
+    </DataportaalBody>
+  </>
+))
