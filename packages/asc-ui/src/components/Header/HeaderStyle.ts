20409d0024146a11625f78a5c83c1f0b2b9d497e
royderks <10717410+royderks@users.noreply.github.com>
2019-09-11 14:51:27 +0200

    use the max width from the grid to define the max width of the header

--- packages/asc-ui/src/components/Header/HeaderStyle.ts
+++ packages/asc-ui/src/components/Header/HeaderStyle.ts
@@ -1,24 +1,12 @@
-import styled, { css } from '@datapunt/asc-core'
+import styled from '@datapunt/asc-core'
 import TopBarStyle from '../TopBar/TopBarStyle'
-import { HEADER_MAX_WIDTH } from '../shared/sizes'
 
-export type HeaderStyleProps = {
-  fullWidth?: boolean
-}
-
-const HeaderStyle = styled(TopBarStyle)<HeaderStyleProps>`
+const HeaderStyle = styled(TopBarStyle)<{}>`
   flex-wrap: nowrap;
   height: 100%;
   margin: 0 auto;
   padding: 0 10px;
   align-items: stretch;
-  max-width: ${({ fullWidth }) =>
-    !fullWidth ? `${HEADER_MAX_WIDTH}px` : 'none'};
-  ${({ fullWidth }) =>
-    fullWidth &&
-    css`
-      padding-left: 10px !important;
-    `}
 `
 
 export default HeaderStyle
