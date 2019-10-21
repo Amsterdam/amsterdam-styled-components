6d018035e09aed2410d6112527a5ebb4fd3614db
Chris van Mook <chris@chrisvanmook.com>
2019-10-21 14:38:42 +0200

    Minor style modification on FilterBox

--- packages/asc-ui/src/components/FilterBox/FilterBoxStyle.ts
+++ packages/asc-ui/src/components/FilterBox/FilterBoxStyle.ts
@@ -6,7 +6,7 @@ import ButtonStyle from '../Button/ButtonStyle'
 export type Props = {}
 
 export default styled.div<Props>`
-  border: 1px solid ${themeColor('tint', 'level3')};
+  border: 2px solid ${themeColor('tint', 'level3')};
   padding: 0 ${themeSpacing(5)} ${themeSpacing(5)};
 
   ${LabelStyle} {
@@ -22,5 +22,5 @@ export const FilterBoxHeader = styled.div`
   margin-bottom: ${themeSpacing(5)};
   padding: ${themeSpacing(4)} 0;
   min-height: 84px;
-  border-bottom: 1px solid ${themeColor('tint', 'level3')};
+  border-bottom: 2px solid ${themeColor('tint', 'level3')};
 `
