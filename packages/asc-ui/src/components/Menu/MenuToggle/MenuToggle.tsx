46c3383e5c0790776d21e9636089907aae66d359
royderks <10717410+royderks@users.noreply.github.com>
2019-10-29 16:11:48 +0100

    Created test change to see if canary is working

--- packages/asc-ui/src/components/Menu/MenuToggle/MenuToggle.tsx
+++ packages/asc-ui/src/components/Menu/MenuToggle/MenuToggle.tsx
@@ -32,28 +32,26 @@ const MenuToggle: React.FC<Props> = ({
   }, [handleOnExpand, menuOpen])
 
   return (
-    <>
-      <MenuContext.Provider
-        value={{
-          underFlyOutMenu: false,
-          hasToggle: true,
-          setOpenToggle: () => {
-            setMenuOpen(false)
-          },
-        }}
+    <MenuContext.Provider
+      value={{
+        underFlyOutMenu: false,
+        hasToggle: true,
+        setOpenToggle: () => {
+          setMenuOpen(false)
+        },
+      }}
+    >
+      <Toggle
+        as={MenuToggleStyle}
+        onOpen={onOpen}
+        onClick={toggleMenu}
+        align={align}
+        open={menuOpen}
+        {...otherProps}
       >
-        <Toggle
-          as={MenuToggleStyle}
-          onOpen={onOpen}
-          onClick={toggleMenu}
-          align={align}
-          open={menuOpen}
-          {...otherProps}
-        >
-          <MenuList>{children}</MenuList>
-        </Toggle>
-      </MenuContext.Provider>
-    </>
+        <MenuList>{children}</MenuList>
+      </Toggle>
+    </MenuContext.Provider>
   )
 }
 
