e2d177b86c38778207107c43d356499187815609
Chris van Mook <chris@chrisvanmook.com>
2019-10-30 10:59:08 +0100

    Modified styles in FilterOptions

--- packages/asc-ui/src/components/FilterOption/FilterOption.tsx
+++ packages/asc-ui/src/components/FilterOption/FilterOption.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import FilterOptionStyle, { Props } from './FilterOptionStyle'
 
-const FilterOption: React.FC<Props & React.HTMLAttributes<HTMLElement>> = ({
-  children,
-  ...otherProps
-}) => <FilterOptionStyle {...otherProps}>{children}</FilterOptionStyle>
+const FilterOption: React.FC<
+  Props & React.AnchorHTMLAttributes<HTMLAnchorElement>
+> = ({ children, ...otherProps }) => (
+  <FilterOptionStyle {...otherProps}>{children}</FilterOptionStyle>
+)
 
 export default FilterOption
