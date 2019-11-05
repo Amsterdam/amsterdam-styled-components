4bb7777b3f26c931dd89df0f4b3b1d22ef6959d2
Thomas Amar <hello@ebabel.eu>
2019-11-05 11:01:30 +0100

    Improve solution based on Chris suggestions.

--- packages/asc-ui/src/components/FilterTag/FilterTag.stories.tsx
+++ packages/asc-ui/src/components/FilterTag/FilterTag.stories.tsx
@@ -2,24 +2,43 @@ import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 import FilterTag from './FilterTag'
 
-const filterTagHandleClick = (e: React.KeyboardEvent | React.MouseEvent) => {
-  const target = e.currentTarget as HTMLElement
-  target.remove()
+const filterTagHandleClick = (nr: number) => {
+  // eslint-disable-next-line no-console
+  console.log(nr)
 }
 
 storiesOf('Atoms/FilterTag', module)
   .addDecorator(storyFn => (
     <div style={{ padding: '40px 20px' }}>{storyFn()}</div>
   ))
-  .add('default', () => (
-    <>
-      <FilterTag onClick={filterTagHandleClick}>
-        Verkeer en infrastructuur
-      </FilterTag>
-      <FilterTag onClick={filterTagHandleClick}>Multiselect</FilterTag>
-      <br />
-      <FilterTag onClick={filterTagHandleClick}>
-        Filters over meerdere regels
-      </FilterTag>
-    </>
-  ))
+  .add('default', () => {
+    const myArray = [
+      {
+        id: 1,
+        labelText: 'Verkeer en infrastructuur',
+        ref: React.useRef(null),
+      },
+      {
+        id: 2,
+        labelText: 'Multiselect',
+        ref: React.useRef(null),
+      },
+      {
+        id: 3,
+        labelText: 'Filters over meerdere regels',
+        ref: React.useRef(null),
+      },
+    ]
+    return (
+      <>
+        {myArray.map(element => (
+          <FilterTag
+            ref={element.ref}
+            onClick={() => filterTagHandleClick(element.id)}
+          >
+            Verkeer en infrastructuur
+          </FilterTag>
+        ))}
+      </>
+    )
+  })
--- packages/asc-ui/src/components/FilterTag/FilterTag.tsx
+++ packages/asc-ui/src/components/FilterTag/FilterTag.tsx
@@ -7,24 +7,16 @@ import Icon from '../Icon'
 
 export interface FilterTagProps extends TagProps {}
 
-const FilterTag: React.FC<FilterTagProps> = ({
-  onClick,
-  children,
-  ...otherProps
-}) => {
-  const handleClick = (e: React.KeyboardEvent | React.MouseEvent) => {
-    onClick(e)
-  }
-
-  return (
-    <FilterTagStyle {...otherProps} onClick={handleClick}>
-      {children}
-      <Icon inline size={14} color="primary">
-        <Close />
-      </Icon>
-    </FilterTagStyle>
-  )
-}
+const FilterTag: React.FC<
+  FilterTagProps & React.ButtonHTMLAttributes<HTMLButtonElement>
+> = ({ children, ...otherProps }) => (
+  <FilterTagStyle $as="button" {...otherProps}>
+    {children}
+    <Icon inline size={14} color="primary">
+      <Close />
+    </Icon>
+  </FilterTagStyle>
+)
 
 FilterTag.defaultProps = Tag.defaultProps
 
