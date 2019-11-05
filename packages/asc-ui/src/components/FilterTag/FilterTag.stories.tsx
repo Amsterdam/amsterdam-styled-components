560b7af46b85ab3e9d863df9ac19445b7de6ed30
Thomas Amar <hello@ebabel.eu>
2019-11-05 17:44:50 +0100

    Remove ref and forwardRef because useState is enough.

--- packages/asc-ui/src/components/FilterTag/FilterTag.stories.tsx
+++ packages/asc-ui/src/components/FilterTag/FilterTag.stories.tsx
@@ -5,7 +5,6 @@ import FilterTag from './FilterTag'
 interface FilterTagElement {
   id: number
   labelText: string
-  ref: React.MutableRefObject<null>
 }
 
 storiesOf('Atoms/FilterTag', module)
@@ -17,17 +16,14 @@ storiesOf('Atoms/FilterTag', module)
       {
         id: 1,
         labelText: 'Verkeer en infrastructuur',
-        ref: React.useRef(null),
       },
       {
         id: 2,
         labelText: 'Multiselect',
-        ref: React.useRef(null),
       },
       {
         id: 3,
         labelText: 'Filters over meerdere regels',
-        ref: React.useRef(null),
       },
     ]
 
@@ -46,7 +42,6 @@ storiesOf('Atoms/FilterTag', module)
         {filterTagElements &&
           filterTagElements.map(element => (
             <FilterTag
-              forwardRef={element.ref}
               onClick={() => filterTagHandleClick(element.id)}
               key={`filterTag${element.id}`}
             >
