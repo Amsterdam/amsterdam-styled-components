f5e6b32c08350ac4e735f138288175303237a404
Thomas Amar <hello@ebabel.eu>
2019-11-11 13:54:46 +0100

    Add story for Accordion, the new component.

---
+++ packages/asc-ui/src/components/Accordion/Accordion.stories.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import { storiesOf } from '@storybook/react'
+import Accordion from './Accordion'
+
+storiesOf('Atoms/Accordion', module)
+  .addDecorator(storyFn => (
+    <div style={{ padding: '40px 20px' }}>{storyFn()}</div>
+  ))
+  .add('default', () => {
+    return (
+      <>
+        <Accordion>Eerste accordion tekst</Accordion>
+        <Accordion>Tweede accordion tekst</Accordion>
+        <Accordion>
+          Accordeon ingeklapt met meer tekst dan dat zichtbaar is in dit
+          resolutie
+        </Accordion>
+      </>
+    )
+  })
