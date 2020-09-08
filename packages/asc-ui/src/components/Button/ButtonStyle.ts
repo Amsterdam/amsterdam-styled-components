132553e7c28b2848819f8cd50a8fb130edb38596
Robin Piets <robinpiets@gmail.com>
2020-09-08 18:24:33 +0200

    Change taskflow button to have correct focus appearance

--- packages/asc-ui/src/components/Button/ButtonStyle.ts
+++ packages/asc-ui/src/components/Button/ButtonStyle.ts
@@ -24,11 +24,12 @@ export const getButtonHeight = (theme: Theme.ThemeInterface) =>
 export const ArrowRight = styled.div`
   position: absolute;
   top: 0;
-  right: -37px;
+  left: 100%;
   width: 0;
   height: 0;
   border: 22px solid rgba(255, 255, 255, 0);
   border-left: 15px solid ${themeColor('secondary')};
+  border-right: 0;
   ${transitions('border-color', '0.1s ease-in-out')}
 `
 
