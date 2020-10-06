c6ce950706741527182c1df28af2457ab2f655f8
Jasper Swart <jasper.g.swart@gmail.com>
2020-10-06 17:49:43 +0200

    added BreadCrumbs

--- packages/asc-ui/src/components/Breadcrumbs/Breadcrumbs.tsx
+++ packages/asc-ui/src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,9 +1,24 @@
 import React, { FunctionComponent, HTMLAttributes } from 'react'
-import BreadcrumbsStyle from './BreadcrumbsStyle'
+import BreadcrumbsStyle, { StyledIcon } from './BreadcrumbsStyle'
+import { ChevronRight } from '@datapunt/asc-assets'
+import { themeColor } from '@datapunt/asc-ui'
 
 const Breadcrumbs: FunctionComponent<HTMLAttributes<HTMLUListElement>> = ({
   children,
   ...otherProps
-}) => <BreadcrumbsStyle {...otherProps}>{children}</BreadcrumbsStyle>
+}) => (
+  <BreadcrumbsStyle {...otherProps}>
+    {React.Children.map(children, (child, index) => (
+      <React.Fragment key={String(index)}>
+        {child}
+        {children instanceof Array && index < children.length - 1 && (
+          <StyledIcon size={14} color={themeColor('tint', 'level5')}>
+            <ChevronRight />
+          </StyledIcon>
+        )}
+      </React.Fragment>
+    ))}
+  </BreadcrumbsStyle>
+)
 
 export default Breadcrumbs
--- packages/asc-ui/src/components/Breadcrumbs/BreadcrumbsStyle.tsx
+++ packages/asc-ui/src/components/Breadcrumbs/BreadcrumbsStyle.tsx
@@ -1,3 +1,16 @@
 import styled from 'styled-components'
+import { Icon, themeSpacing, breakpoint } from '@datapunt/asc-ui'
 
-export default styled.ul``
+export default styled.ul`
+  display: flex;
+  align-items: center;
+  margin: ${themeSpacing(4)} 0;
+
+  @media ${breakpoint('min-width', 'laptopM')} {
+    margin: ${themeSpacing(5)} 0;
+  }
+`
+
+export const StyledIcon = styled(Icon)`
+  margin: 0 ${themeSpacing(1)};
+`
