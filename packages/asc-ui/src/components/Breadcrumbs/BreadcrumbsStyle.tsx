483748726ac0b9a0cadc3433c18912b46b9bc1e2
Jasper Swart <jasper.g.swart@gmail.com>
2020-10-08 14:36:50 +0200

    added flex wrapping and white-space nowrap

--- packages/asc-ui/src/components/Breadcrumbs/BreadcrumbsStyle.tsx
+++ packages/asc-ui/src/components/Breadcrumbs/BreadcrumbsStyle.tsx
@@ -5,6 +5,7 @@ import { themeSpacing, themeColor, breakpoint } from '../../utils/themeUtils'
 export default styled.ul`
   display: flex;
   align-items: center;
+  flex-wrap: wrap;
   margin: ${themeSpacing(4)} 0;
 
   svg {
