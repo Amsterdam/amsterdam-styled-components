d3e0d40d2b87db9d7c3428154f913fe16b20ce72
Jasper Swart <jasper.g.swart@gmail.com>
2020-10-14 11:23:59 +0200

    fixed styling and Fragment

--- packages/asc-ui/src/components/Breadcrumbs/BreadcrumbsStyle.tsx
+++ packages/asc-ui/src/components/Breadcrumbs/BreadcrumbsStyle.tsx
@@ -1,11 +1,11 @@
 import styled from 'styled-components'
-import { themeSpacing, themeColor } from '../../utils/themeUtils'
+import { themeColor } from '../../utils/themeUtils'
 
 export default styled.ul`
   display: flex;
   align-items: center;
   flex-wrap: wrap;
-  margin: ${themeSpacing(4)} 0;
+  margin: 0;
 
   li:first-child span {
     display: none;
