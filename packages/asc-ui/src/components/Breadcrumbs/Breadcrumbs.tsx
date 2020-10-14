d70ea143eaa50183d62f77be6ab83c698a930f00
Jasper Swart <jasper.g.swart@gmail.com>
2020-10-14 11:23:59 +0200

    fixed styling and Fragment

--- packages/asc-ui/src/components/Breadcrumbs/Breadcrumbs.tsx
+++ packages/asc-ui/src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, HTMLAttributes } from 'react'
+import React, { Fragment, FunctionComponent, HTMLAttributes } from 'react'
 import BreadcrumbsStyle from './BreadcrumbsStyle'
 
 const Breadcrumbs: FunctionComponent<HTMLAttributes<HTMLUListElement>> = ({
@@ -7,7 +7,7 @@ const Breadcrumbs: FunctionComponent<HTMLAttributes<HTMLUListElement>> = ({
   ...otherProps
 }) => (
   <BreadcrumbsStyle className={className} {...otherProps}>
-    <React.Fragment>{children}</React.Fragment>
+    <Fragment>{children}</Fragment>
   </BreadcrumbsStyle>
 )
 
