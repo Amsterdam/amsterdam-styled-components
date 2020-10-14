76b32d8bcf5a4febfaeec70055eb7e02d45490a6
Jasper Swart <jasper.g.swart@gmail.com>
2020-10-14 10:20:15 +0200

    added test for Breadcrumbs

--- packages/asc-ui/src/components/Breadcrumbs/Breadcrumbs.test.tsx
+++ packages/asc-ui/src/components/Breadcrumbs/Breadcrumbs.test.tsx
@@ -1,10 +1,23 @@
 import { render } from '@testing-library/react'
 import React from 'react'
 import Breadcrumbs from './Breadcrumbs'
+import BreadcrumbsItem from './BreadcrumbsItem/BreadcrumbsItem'
 
 describe('Breadcrumbs', () => {
   it('should render', () => {
-    const { container } = render(<Breadcrumbs>Foo</Breadcrumbs>)
+    const { container } = render(
+      <Breadcrumbs>
+        <BreadcrumbsItem href="/">Home</BreadcrumbsItem>
+        <BreadcrumbsItem href="/innovatie">Innovatie</BreadcrumbsItem>
+        <BreadcrumbsItem href="/innovatie/bouwprojecten">
+          Bouw- en verkkeersprojecten
+        </BreadcrumbsItem>
+        <BreadcrumbsItem href="/innovatie/bouwprojecten/contact">
+          Contact
+        </BreadcrumbsItem>
+      </Breadcrumbs>,
+    )
+
     expect(container.firstChild).toBeDefined()
   })
 })
