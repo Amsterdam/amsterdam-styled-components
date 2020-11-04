36deed54272641e944d093730334247d3573fbf9
Jon Koops <jonkoops@gmail.com>
2020-11-04 16:57:38 +0100

    Clean up compact pager example

--- stories/src/ui/CompactPagerExample.tsx
+++ stories/src/ui/CompactPagerExample.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import React, { FunctionComponent, useState } from 'react'
 import { action } from '@storybook/addon-actions'
 import { CompactPager } from '@amsterdam/asc-ui'
 
-const CompactPagerExample: React.FC<React.HTMLAttributes<HTMLElement>> = () => {
-  const [currentPage, setCurrentPage] = useState<number>(1)
+const CompactPagerExample: FunctionComponent = () => {
+  const [currentPage, setCurrentPage] = useState(1)
+
   return (
     <CompactPager
       page={currentPage}
