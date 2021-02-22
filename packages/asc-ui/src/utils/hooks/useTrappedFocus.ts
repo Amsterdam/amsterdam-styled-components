6b0e35a4b78775478577273e400338db676b1a09
Aram Limpens <a.limpens@amsterdam.nl>
2021-02-22 15:28:50 +0100

    Include all non-disabled inputs in list of focusable elements

--- packages/asc-ui/src/utils/hooks/useTrappedFocus.ts
+++ packages/asc-ui/src/utils/hooks/useTrappedFocus.ts
@@ -6,9 +6,7 @@ export const FocusableElements = [
   'a[href]:not([disabled])',
   'button:not([disabled])',
   'textarea:not([disabled])',
-  'input[type="text"]:not([disabled])',
-  'input[type="radio"]:not([disabled])',
-  'input[type="checkbox"]:not([disabled])',
+  'input:not([disabled])',
   'select:not([disabled])',
 ]
 
