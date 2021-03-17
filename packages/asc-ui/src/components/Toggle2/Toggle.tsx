af2b93986bd1a514695b7d15953d5a4ce3ff595e
Jasper Swart <jasper.g.swart@gmail.com>
2021-03-17 12:55:17 +0100

    added defaultValue to Toggle

--- packages/asc-ui/src/components/Toggle2/Toggle.tsx
+++ packages/asc-ui/src/components/Toggle2/Toggle.tsx
@@ -4,32 +4,39 @@ import RadioStyle, { Props, LabelStyle, WrapperStyle } from './ToggleStyle'
 const Toggle = forwardRef<
   HTMLInputElement,
   Props & HTMLAttributes<HTMLInputElement>
->(({ id, left, right, className, ...otherProps }, externalRef) => {
-  const ref = useRef<HTMLInputElement>(null)
+>(
+  (
+    { id, left, right, defaultValue, className, ...otherProps },
+    externalRef,
+  ) => {
+    const ref = useRef<HTMLInputElement>(null)
 
-  useImperativeHandle(externalRef, () => ref.current as HTMLInputElement)
+    useImperativeHandle(externalRef, () => ref.current as HTMLInputElement)
 
-  return (
-    <WrapperStyle className={className}>
-      <RadioStyle
-        role="switch"
-        name={id}
-        id={left}
-        value={left}
-        {...{ ...otherProps }}
-      />
-      <LabelStyle htmlFor={left}>{left}</LabelStyle>
+    return (
+      <WrapperStyle className={className}>
+        <RadioStyle
+          role="switch"
+          name={id}
+          id={left}
+          value={left}
+          defaultChecked={defaultValue === left}
+          {...{ ...otherProps }}
+        />
+        <LabelStyle htmlFor={left}>{left}</LabelStyle>
 
-      <RadioStyle
-        role="switch"
-        name={id}
-        id={right}
-        value={right}
-        {...{ ...otherProps }}
-      />
-      <LabelStyle htmlFor={right}>{right} </LabelStyle>
-    </WrapperStyle>
-  )
-})
+        <RadioStyle
+          role="switch"
+          name={id}
+          id={right}
+          value={right}
+          defaultChecked={defaultValue === right}
+          {...{ ...otherProps }}
+        />
+        <LabelStyle htmlFor={right}>{right} </LabelStyle>
+      </WrapperStyle>
+    )
+  },
+)
 
 export default Toggle
--- packages/asc-ui/src/components/Toggle2/ToggleStyle.ts
+++ packages/asc-ui/src/components/Toggle2/ToggleStyle.ts
@@ -5,6 +5,7 @@ export type Props = {
   id: string
   left: string
   right: string
+  defaultValue: string
   className?: string
 }
 
@@ -30,7 +31,7 @@ type LabelProps = {
 
 export const LabelStyle = styled.label<LabelProps>`
   background-color: ${themeColor('tint', 'level3')};
-  padding: 20px;
+  padding: 10px 20px;
 `
 
 export const KnobStyle = styled.span`
