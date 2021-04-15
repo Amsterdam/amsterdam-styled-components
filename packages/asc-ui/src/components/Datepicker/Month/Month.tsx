7169fc6ce6dcc3fffde9d831dea5a6ff1cb70008
Jasper Swart <jasper.g.swart@gmail.com>
2021-04-15 15:34:27 +0200

    added callbacks for Month prev and next

--- packages/asc-ui/src/components/Datepicker/Month/Month.tsx
+++ packages/asc-ui/src/components/Datepicker/Month/Month.tsx
@@ -11,7 +11,7 @@ import MonthStyle, {
 } from './MonthStyle'
 import { weekDays, months, daysInMonth } from '../../shared/constants'
 
-const Month: FunctionComponent<Props> = ({ date }) => {
+const Month: FunctionComponent<Props> = () => {
   const [firstDay, setFirstDay] = useState<any>(new Date())
   const [month, setMonth] = useState<number>(new Date().getMonth())
   const [year, setYear] = useState<number>(new Date().getFullYear())
@@ -28,6 +28,8 @@ const Month: FunctionComponent<Props> = ({ date }) => {
   const renderDays = () => {
     const days: any = []
 
+    // @TODO  added support for schrikkeljaar
+
     for (let i = 0; i < firstDay.getDay() - 1; i++) {
       days.push({ number: '*', key: days.length })
     }
@@ -42,9 +44,23 @@ const Month: FunctionComponent<Props> = ({ date }) => {
         days.push({ number: i, key: days.length })
       }
     }
-    console.log('renderDays', firstDay.getDay())
+    console.log('renderDays', days)
 
     setAllDays(days)
+  }
+
+  const onPrevious = () => {
+    console.log('onPrevious')
+    e.preventDefault()
+    setMonth(month - 1)
+    renderDays()
+  }
+
+  const onNext = () => {
+    console.log('onNext')
+    e.preventDefault()
+    setMonth(month + 1)
+    renderDays()
   }
 
   console.log('Month ', allDays)
@@ -52,14 +68,7 @@ const Month: FunctionComponent<Props> = ({ date }) => {
   return (
     <MonthStyle>
       <Header>
-        <NextPrev
-          href="/"
-          variant="blank"
-          onClick={(e: any) => {
-            e.preventDefault()
-            console.log('previous month')
-          }}
-        >
+        <NextPrev href="/" variant="blank" onClick={(e: any) => onPrevious(e)}>
           <Icon>
             <ChevronLeft />
           </Icon>
@@ -67,14 +76,7 @@ const Month: FunctionComponent<Props> = ({ date }) => {
         <Title>
           {months[month]} {year}
         </Title>
-        <NextPrev
-          href="/"
-          variant="blank"
-          onClick={(e: any) => {
-            e.preventDefault()
-            console.log('next month')
-          }}
-        >
+        <NextPrev href="/" variant="blank" onClick={(e: any) => onNext(e)}>
           <Icon>
             <ChevronRight />
           </Icon>
