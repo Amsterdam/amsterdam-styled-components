278330125eb21e21d9e21bb577185a9a707c1aed
Jasper Swart <jasper.g.swart@gmail.com>
2021-04-20 13:47:11 +0200

    added type for Datepicker

--- packages/asc-ui/src/components/Datepicker/Datepicker.tsx
+++ packages/asc-ui/src/components/Datepicker/Datepicker.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useCallback } from 'react'
+import { FunctionComponent, useCallback, HTMLAttributes } from 'react'
 import DatepickerStyle, { Props } from './DatepickerStyle'
 import Month from './Month'
 
@@ -6,7 +6,9 @@ import Month from './Month'
 // @TODO add text input element
 // @TODO add support date pushing down to Month
 
-const Datepicker: FunctionComponent<Props> = ({ value }) => {
+const Datepicker: FunctionComponent<Props & HTMLAttributes<HTMLDivElement>> = ({
+  value,
+}) => {
   console.log('Datepicker', value)
   // const newDate = '12-03-2021'
   const onClickDay = useCallback((thisDate: string) => {
