85790c6e27180ed6f99d216193582ac969ec6988
Jasper Swart <jasper.g.swart@gmail.com>
2021-04-23 15:56:56 +0200

    added more unit tests for Month

--- packages/asc-ui/src/components/Datepicker/Month/Month.test.tsx
+++ packages/asc-ui/src/components/Datepicker/Month/Month.test.tsx
@@ -3,11 +3,25 @@ import Month from './Month'
 import { ThemeProvider } from '../../../theme'
 
 describe('Month', () => {
-  it('should render', () => {
+  it('should not render when closed', () => {
     const onSelectDay = jest.fn()
     render(
       <ThemeProvider>
-        <Month open date="15-04-2019" onSelectDay={onSelectDay} />
+        <Month date="15-04-2019" onSelectDay={onSelectDay} />
+      </ThemeProvider>,
+    )
+
+    expect(screen.queryByTestId('month')).not.toBeInTheDocument()
+    expect(screen.queryByTestId('previous')).not.toBeInTheDocument()
+    expect(screen.queryByTestId('next')).not.toBeInTheDocument()
+    expect(screen.queryAllByTestId('weekday').length).toBe(0)
+  })
+
+  it('should render when open rendering a 4 week month', () => {
+    const onSelectDay = jest.fn()
+    render(
+      <ThemeProvider>
+        <Month open date="12-02-2021" onSelectDay={onSelectDay} />
       </ThemeProvider>,
     )
 
@@ -15,5 +29,28 @@ describe('Month', () => {
     expect(screen.queryByTestId('previous')).toBeInTheDocument()
     expect(screen.queryByTestId('next')).toBeInTheDocument()
     expect(screen.queryAllByTestId('weekday').length).toBe(7)
+    expect(screen.queryAllByTestId('day').length).toBe(28)
+  })
+
+  it('should render when open rendering a 5 week month', () => {
+    const onSelectDay = jest.fn()
+    render(
+      <ThemeProvider>
+        <Month open date="12-04-2021" onSelectDay={onSelectDay} />
+      </ThemeProvider>,
+    )
+
+    expect(screen.queryAllByTestId('day').length).toBe(35)
+  })
+
+  it('should render when open rendering a 6 week month', () => {
+    const onSelectDay = jest.fn()
+    render(
+      <ThemeProvider>
+        <Month open date="12-05-2021" onSelectDay={onSelectDay} />
+      </ThemeProvider>,
+    )
+
+    expect(screen.queryAllByTestId('day').length).toBe(42)
   })
 })
