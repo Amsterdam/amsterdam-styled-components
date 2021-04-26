af3f883914a52b30c04445d01541831ed7f3a6b9
Jasper Swart <jasper.g.swart@gmail.com>
2021-04-26 12:30:09 +0200

    moved useClickOutside to shared/hooks

--- packages/asc-ui/src/components/Datepicker/Datepicker.tsx
+++ packages/asc-ui/src/components/Datepicker/Datepicker.tsx
@@ -7,7 +7,7 @@ import {
   useState,
 } from 'react'
 import { Calendar } from '@amsterdam/asc-assets'
-import useClickOutside from './Month/useClickOutside'
+import useClickOutside from '../../utils/hooks/useClickOutside'
 import DatepickerStyle, {
   Props,
   StyledInput,
