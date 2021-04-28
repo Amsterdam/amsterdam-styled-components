c06d5d98d7d4c3944d5a622f607fa2537c8770c0
Jasper Swart <jasper.g.swart@gmail.com>
2021-04-28 16:30:15 +0200

    renamed to handler

--- packages/asc-ui/src/components/Datepicker/useClickOutside.tsx
+++ packages/asc-ui/src/components/Datepicker/useClickOutside.tsx
@@ -3,26 +3,27 @@ import { useEffect } from 'react'
 /**
  * Hook that alerts clicks outside of the passed ref
  */
-const useClickOutside = (ref: any, callback: () => void) => {
+const useClickOutside = (ref: any, handler: () => void) => {
   useEffect(() => {
     /**
      * Alert if clicked on outside of element
      */
     const handleClickOutside = (event: any) => {
       if (ref.current && !ref.current.contains(event.target)) {
-        if (callback) {
-          callback()
+        if (typeof handler === 'function') {
+          handler()
         }
       }
     }
 
     // Bind the event listener
+    
     document.addEventListener('mouseup', handleClickOutside)
     return () => {
       // Unbind the event listener on clean up
       document.removeEventListener('mouseup', handleClickOutside)
     }
-  }, [ref, callback])
+  }, [ref, handler])
 }
 
 export default useClickOutside
