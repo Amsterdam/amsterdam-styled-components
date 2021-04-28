c37ab86522f6273049bf83bbeaff4fbc854debbf
Jasper Swart <jasper.g.swart@gmail.com>
2021-04-28 12:30:52 +0200

    moved useClickOutside

---
+++ packages/asc-ui/src/components/Datepicker/useClickOutside.test.tsx
@@ -0,0 +1,32 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+// import { renderHook } from '@testing-library/react-hooks'
+import { useRef } from 'react'
+import useClickOutside from './useClickOutside'
+
+describe('useClickOutside', () => {
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('should focus the element', () => {
+    const isOutside = jest.fn()
+    const wrapperRef = useRef(null)
+
+    useClickOutside(wrapperRef, isOutside)
+
+    render(
+      <>
+        <div ref={wrapperRef}>
+          <span data-testid="inside">inside</span>
+        </div>
+        <span data-testid="outside">outside</span>
+      </>,
+    )
+
+    fireEvent.click(screen.getByTestId('inside'))
+    expect(isOutside).toHaveBeenCalledTimes(0)
+
+    fireEvent.click(screen.getByTestId('outside'))
+    expect(isOutside).toHaveBeenCalledTimes(1)
+  })
+})
