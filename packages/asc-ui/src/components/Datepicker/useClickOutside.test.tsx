6ebd822e52b7d0db04fcfecd9777a89579fc03a9
Jasper Swart <jasper.g.swart@gmail.com>
2021-04-28 17:51:40 +0200

    fixed test  of useClickOutside

--- packages/asc-ui/src/components/Datepicker/useClickOutside.test.tsx
+++ packages/asc-ui/src/components/Datepicker/useClickOutside.test.tsx
@@ -9,12 +9,10 @@ describe('useClickOutside', () => {
   })
 
   it('should register click inside and outside the wrapper', () => {
-    // @TODO fix error in test
-
     const isOutside = jest.fn()
     const wrapperRef = createRef<HTMLDivElement>()
 
-    renderHook(() => useClickOutside(wrapperRef, isOutside))
+    renderHook(() => useClickOutside(wrapperRef, () => isOutside()))
 
     render(
       <>
@@ -25,10 +23,10 @@ describe('useClickOutside', () => {
       </>,
     )
 
-    fireEvent.click(screen.getByTestId('inside'))
+    fireEvent.mouseUp(screen.getByTestId('inside'))
     expect(isOutside).toHaveBeenCalledTimes(0)
 
-    // fireEvent.click(screen.getByTestId('outside'))
-    // expect(isOutside).toHaveBeenCalledTimes(1)
+    fireEvent.mouseUp(screen.getByTestId('outside'))
+    expect(isOutside).toHaveBeenCalledTimes(1)
   })
 })
