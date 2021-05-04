e761a165d93e22dfc6bef1a731660c765da2d570
Jasper Swart <jasper.g.swart@gmail.com>
2021-05-04 11:14:47 +0200

    added MenuContext to test

--- packages/asc-ui/src/components/Menu/MenuFlyOut/MenuFlyOut.new.test.tsx
+++ packages/asc-ui/src/components/Menu/MenuFlyOut/MenuFlyOut.new.test.tsx
@@ -2,24 +2,27 @@ import { render, screen, fireEvent, act } from '@testing-library/react'
 import MenuInline from '../MenuInline'
 import MenuButton from '../MenuButton'
 import MenuItem from '../MenuItem'
+import MenuContext from '../MenuContext'
 import MenuFlyOut from './MenuFlyOut'
 
 describe('MenuFlyOut', () => {
   it('should render', () => {
     const { container } = render(
       <MenuInline>
-        <MenuFlyOut label="Submenu">
-          <MenuItem>
-            <MenuButton forwardedAs="a" href="/">
-              Link 1
-            </MenuButton>
-          </MenuItem>
-          <MenuItem>
-            <MenuButton forwardedAs="a" href="/">
-              Link2
-            </MenuButton>
-          </MenuItem>
-        </MenuFlyOut>
+        <MenuContext.Provider value={{ hasToggle: true }}>
+          <MenuFlyOut label="Submenu">
+            <MenuItem>
+              <MenuButton forwardedAs="a" href="/">
+                Link 1
+              </MenuButton>
+            </MenuItem>
+            <MenuItem>
+              <MenuButton forwardedAs="a" href="/">
+                Link2
+              </MenuButton>
+            </MenuItem>
+          </MenuFlyOut>
+        </MenuContext.Provider>
       </MenuInline>,
     )
 
