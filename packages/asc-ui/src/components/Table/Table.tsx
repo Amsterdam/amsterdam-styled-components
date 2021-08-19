e5412053695e73ad50e807a47892189a4af9e2e2
Chris van Mook <chrisvanmook@gmail.com>
2021-08-19 11:49:58 +0200

    Fixed: default 100% width for `Table` component

--- packages/asc-ui/src/components/Table/Table.tsx
+++ packages/asc-ui/src/components/Table/Table.tsx
@@ -2,7 +2,8 @@ import styled from 'styled-components'
 import { themeSpacing } from '../../utils'
 
 const Table = styled.table`
-  display: inline-block;
+  display: table;
+  width: 100%;
   border-spacing: 0;
   overflow: auto;
   padding-right: ${themeSpacing(3)};
