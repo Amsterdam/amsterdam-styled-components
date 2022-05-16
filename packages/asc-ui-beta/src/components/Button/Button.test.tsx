4021ddb7f3f5dec57dcba84ece1d19a161d2d0cc
Aram <37216945+alimpens@users.noreply.github.com>
2022-05-16 09:41:48 +0200

    Remove beta package from main branch (#2123)

--- packages/asc-ui-beta/src/components/Button/Button.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-import Button from './Button'
-
-describe('Button', () => {
-  it('should render the button', () => {
-    const { container } = render(<Button>Hello</Button>)
-    expect(container.firstChild).toBeDefined()
-  })
-})
--- packages/asc-ui-beta/src/index.ts
+++
@@ -1,6 +0,0 @@
-import ascBetaTheme from './theme/ascBetaTheme'
-import Button from './components/Button'
-import Heading from './components/Heading'
-import Paragraph from './components/Paragraph'
-
-export { ascBetaTheme, Button, Heading, Paragraph }
