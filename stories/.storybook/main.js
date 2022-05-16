1816709d93f990ac9d085aac2db6456a4e8b47de
Aram Limpens <a.limpens@amsterdam.nl>
2022-05-16 10:52:17 +0200

    Fix storybook addon order warning

--- stories/.storybook/main.js
+++ stories/.storybook/main.js
@@ -1,11 +1,11 @@
 module.exports = {
   stories: ['../src/**/*.stories.@(tsx|mdx)'],
   addons: [
+    '@storybook/addon-docs',
     '@storybook/addon-links',
     '@storybook/addon-controls',
     '@storybook/addon-a11y',
     '@storybook/addon-actions',
-    '@storybook/addon-docs',
     '@storybook/addon-viewport',
   ],
 }
